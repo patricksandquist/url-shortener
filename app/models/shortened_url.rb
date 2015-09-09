f8ab5cd44a2c37749dbfeccb13ae53cae4c430f4
Patrick Sandquist <patricksandquist@gmail.com>
2015-09-09 14:17:42 -0700

    Associated users w shortened urls

---
+++ app/models/shortened_url.rb
@@ -0,0 +1,27 @@
+class ShortenedUrl < ActiveRecord::Base
+  validates :submitter_id, presence: true
+  validates :short_url, presence: true, uniqueness: true
+
+  belongs_to(
+    :submitter,
+    class_name: "User",
+    foreign_key: :submitter_id,
+    primary_key: :id
+  )
+
+  def self.create_for_user_and_long_url!(user, long_url)
+    ShortenedUrl.create!(
+      submitter_id: user.id,
+      long_url: long_url,
+      short_url: ShortenedUrl.random_code
+    )
+  end
+
+  def self.random_code
+    a = SecureRandom.urlsafe_base64
+    while exists?(a)
+      a = SecureRandom.urlsafe_base64
+    end
+    a
+  end
+end
